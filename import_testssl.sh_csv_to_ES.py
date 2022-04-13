3229b4977920f1cc913e2a889480622927045d8e
Kelvin Smith <kelvin@thesmithcave.nz>
2022-04-13 19:42:20 +1200

    Updated script

--- import_testssl.sh_csv_to_ES.py
+++ import_testssl.sh_csv_to_ES.py
@@ -10,12 +10,16 @@ from datetime import datetime
 argparser = argparse.ArgumentParser(description="Import testssl.sh CSV logs into ElasticSearch")
 argparser.add_argument("--elasticsearch", "-e", nargs="*", default="localhost", help="ElasticSearch host (default: %(default)s)")
 argparser.add_argument("--index", "-i", default="testssl-scan", help="ElasticSearch index (default: %(default)s)")
+argparser.add_argument("--ca_cert", "-c", help="ElasticSearch CA certificate")
+argparser.add_argument("--user", "-u", default="elastic", help="Username")
+argparser.add_argument("--password", "-p", help="Password")
 argparser.add_argument("files", nargs="+", help="List of testssl.sh logs in CSV format")
 args = argparser.parse_args()
 
-connections.create_connection(hosts=args.elasticsearch)
+http_auth = (args.user, args.password)
+connections.create_connection(hosts=args.elasticsearch,ca_certs=args.ca_cert,use_ssl=True, verify_certs=False, http_auth=http_auth)
 idx = Index(args.index)
-idx.doc_type(DocTestSSLResult)
+idx.document(DocTestSSLResult)
 DocTestSSLResult.init()
 try:
     idx.create()
